188a054202e194f601b836dd13b1e676560b0bd2
Divya <divyareddy0205@gmail.com>
2023-04-24 11:25:56 -0700

    Started working on a Frontend

--- Books-n-Things/pages/index.js
+++ Books-n-Things/pages/index.js
@@ -4,6 +4,17 @@ const Home = () => {
   return (
     <>
     HeroBanner
+    <div>
+      <h2>Best Selling Books in India</h2>
+      <p>Books of many variations</p>
+    </div>
+    <div>
+      {
+        ['Product1', 'Product2'].map((product, index) => product )
+      }
+
+      Footer
+    </div>
     </>
   )
 }
