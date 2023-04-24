cf04fe01cf099aa54ebda9580b61c49b6cfa6e6d
Divya <divyareddy0205@gmail.com>
2023-04-24 12:18:23 -0700

    Started working on a Frontend

--- Books-n-Things/pages/_app.js
+++ Books-n-Things/pages/_app.js
@@ -1,20 +1,11 @@
 import React from 'react';
 import { Toaster } from 'react-hot-toast';
 
-import { Layout } from '../components';
 import '../styles/globals.css';
-import { StateContext } from '../context/StateContext';
 
 function MyApp({ Component, pageProps }) {
   return (
-    // <StateContext>
-    //   <Layout>
-    //     <Toaster />
-    //     <Component {...pageProps} />
-    //   </Layout>
-    // </StateContext>
-    <>
-    </>
+     <Component {...pageProps} />
   )
 }
 
