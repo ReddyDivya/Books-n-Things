de81ac0d4eb26d9dae9a03e1b4c3be048843dd8d
Divya <divyareddy0205@gmail.com>
2023-04-24 12:41:05 -0700

    sanity

--- Books-n-Things/lib/client.js
+++ Books-n-Things/lib/client.js
@@ -1,3 +1,11 @@
 import sanityClient from '@sanity/Client';
 import imageUrlBuilder from '@sanity/image-url';
 
+//sanityClient is a fun which takes object as a parameter
+const client = sanityClient({
+    projectId : 'r58g95hz',
+    dataset:'production',
+    apiVersion:'2023-04-24', //current date
+    useCdn: true,
+    token: process.env.NEXT_PUBLIC_SANITY_TOKEN //for security concerns using Macros
+})
