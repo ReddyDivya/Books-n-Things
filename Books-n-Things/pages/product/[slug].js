4eb1bb37d79d91c1e620d9ee6571f0ceb5e17764
Divya <divyareddy0205@gmail.com>
2023-04-25 14:51:31 -0700

    slug

--- Books-n-Things/pages/product/[slug].js
+++ Books-n-Things/pages/product/[slug].js
@@ -24,7 +24,7 @@ const ProductDetails = ({product, products}) => {
                     }
                 </div> */}
             </div>
-            <div className="product-details-desc">
+            <div className="product-detail-desc">
                 <h1>{name}</h1>
                 <div className="reviews">
                     <div>
@@ -40,6 +40,32 @@ const ProductDetails = ({product, products}) => {
                 </div>
                 <h4>Details:</h4>
                 <p>{details}</p>
+                <p className="price">₹{price}</p>
+                <div className='quantity'>
+                    <h3>Quantity:</h3>
+                    <p className="quantity-desc">
+                        <span className="minus"
+                        onClick=""><AiOutlineMinus/></span>
+                        <span className="num"
+                        onClick="">0</span>
+                        <span className="plus"
+                        onClick=""><AiOutlinePlus/></span>
+                    </p>
+                </div>
+                <div className="buttons">
+                    <button type="button" className="add-to-cart" onClick="">
+                        Add to Cart
+                    </button>
+                    <button type="button" className="buy-now" onClick="">
+                        Buy Now
+                    </button>
+                </div>
+            </div>
+        </div>
+        <div className="maylike-products-wrapper">
+            <h2>You may also like</h2>
+            <div className="marquee">
+                
             </div>
         </div>
     </div>
