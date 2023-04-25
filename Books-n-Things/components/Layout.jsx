e49ae60eae1230f3a31bdc679f246a7faef61563
Divya <divyareddy0205@gmail.com>
2023-04-25 10:40:51 -0700

    layout

--- Books-n-Things/components/Layout.jsx
+++ Books-n-Things/components/Layout.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import Head from 'next/head'; //head is same as in HTML. It gives some metadata about website.
 
 const Layout = () => {
 
   return (
-    <div>
-      Layout
+    <div className="layout">
+        <Head>
+           <title>Books-n-Things Store</title>
+        </Head>
+        {/*header - html5 header tag*/}
+        <header>
+            <Navbar/>
+        </header>
     </div>
   )
 }
