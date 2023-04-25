d6f4fdff6b24e78c57fa93700ac37afd7204c763
Divya <divyareddy0205@gmail.com>
2023-04-25 10:57:49 -0700

    layout

--- Books-n-Things/components/Layout.jsx
+++ Books-n-Things/components/Layout.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Head from 'next/head'; //head is same as in HTML. It gives some metadata about website.
 import Navbar from './Navbar';
+import Footer from './Footer';
 
 const Layout = () => {
 
@@ -13,10 +14,16 @@ const Layout = () => {
         <header>
             <Navbar/>
         </header>
-        {/*header - html5 header tag*/}
-        <main className="main-container">
 
+        {/*main - html5 header tag*/}
+        <main className="main-container">
+          EMPTY
         </main>
+
+        {/*footer - html5 header tag*/}
+        <footer>
+            <Footer/>
+        </footer>
     </div>
   )
 }
