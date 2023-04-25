43cf8435a98811dd854ab55b9e80ab8abf8e1c7f
Divya <divyareddy0205@gmail.com>
2023-04-25 15:00:33 -0700

    slug

--- Books-n-Things/pages/product/[slug].js
+++ Books-n-Things/pages/product/[slug].js
@@ -7,23 +7,25 @@ import { Product } from '../../components';
 const ProductDetails = ({product, products}) => {
     
   const {image, name, details, price} = product;
+  const [index, setIndex] = useState(0);
 
   return (
     <div>
         <div className="product-detail-container">
             <div>
                 <div className="image-container">
-                    <img src={urlFor(image && image[0])} 
-                    />    
+                    <img className="product-detail-image"
+                    src={urlFor(image && image[index])}/>    
                 </div>
-                {/* <div className="small-images-container">
+                <div className="small-images-container">
                     {
                         image?.map((item, i) => (
                             <img src={urlFor(item)} 
-                            className="" onMouseEnter=""/>
+                            className={i === index ? 'small-image selected-image' : 'small-image'} 
+                            onMouseEnter={() => setIndex(i)}/>
                         ))
                     }
-                </div> */}
+                </div>
             </div>
             <div className="product-detail-desc">
                 <h1>{name}</h1>
