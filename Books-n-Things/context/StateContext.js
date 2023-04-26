56fa5430c07b9d3f352e1f3bbac17862298333bf
Divya <divyareddy0205@gmail.com>
2023-04-25 19:33:25 -0700

    add to cart

--- Books-n-Things/context/StateContext.js
+++ Books-n-Things/context/StateContext.js
@@ -7,7 +7,7 @@ const Context = createContext();
 const StateContext = ({children}) => {
 
   const [showCart, setShowCart] = useState(false);
-  const [cartItems, setCartItems] = useState();
+  const [cartItems, setCartItems] = useState([]);
   const [totalPrice, setTotalPrice] = useState();
   const [totalQuantities, setTotalQuantities] = useState();
   const [qty, setQty] = useState(1);
@@ -15,6 +15,27 @@ const StateContext = ({children}) => {
   const onAdd = (product, quantity) => {
     const checkProductInCart = cartItems.find((item) => item._id === product._id)
     
+    if(checkProductInCart){
+      setTotalPrice((prevTotalPrice) => prevTotalPrice + product.price * quantity);
+      setTotalPrice((prevTotalQuantities) => prevTotalQuantities + totalQuantities);
+
+      //updating cart items
+      const updatedCartItems = cartItems.map((cartProduct) => {
+          if(cartProduct._id === product._id)
+          return {
+            ...cartProduct, 
+            quantity: cartProduct.quantity + quantity
+          }
+      })
+
+      setCartItems(updatedCartItems);
+    }
+    else{
+      toast.success(`${qty} ${product.name} added to the cart.`);
+      product.quantity = quantity;
+
+      setCartItems([...cartItems, {...product}]);
+    }
     
   }
 
@@ -45,7 +66,8 @@ const StateContext = ({children}) => {
         totalQuantities,
         qty,
         incQty,
-        decQty
+        decQty,
+        onAdd
       }}
       {children}
     </Context.Provider>
