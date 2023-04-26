a252eacec1434f20b54d8ee8490fc8bf51a6a80e
Divya <divyareddy0205@gmail.com>
2023-04-25 17:25:26 -0700

    state context

--- Books-n-Things/context/StateContext.js
+++ Books-n-Things/context/StateContext.js
@@ -1,6 +1,16 @@
-import React from 'react';
+//Maintains state 
+import React, {createContext, useContext, useState, useEffect} from 'react';
+import {toast} from 'react-hot-toast';
 
-const StateContext = () => {
+const Context = createContext();
+
+const StateContext = ({children}) => {
+
+  const [showCart, setShowCart] = useState(false);
+  const [cartItems, setCartItems] = useState();
+  const [totalPrice, setTotalPrice] = useState();
+  const [totalQuantities, setTotalQuantities] = useState();
+  const [qty, setQty] = useState(1);
 
   return (
     <div>
