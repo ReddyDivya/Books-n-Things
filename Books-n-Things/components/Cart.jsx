f7fcfbd14c5b82a9a52f78f774ab73a1d3e0a110
Divya <divyareddy0205@gmail.com>
2023-04-26 18:30:13 -0700

    working on add to cart

--- Books-n-Things/components/Cart.jsx
+++ Books-n-Things/components/Cart.jsx
@@ -1,36 +1,23 @@
-import React, { useRef } from 'react';
-import Link from 'next/link';
-import { AiOutlineMinus, AiOutlinePlus, AiOutlineLeft, AiOutlineShopping } from 'react-icons/ai';
-import { TiDeleteOutline } from 'react-icons/ti';
+import React, {useRef} from 'react';
+import Link from 'next/Link';
+import {AiOutlineMinus, AiOutlinePlus, AiOutlineShopping, AiOutlineLeft} from 'react-icons/ai';
+import {TiDeleteOutline} from 'react-icons/ti';
 import toast from 'react-hot-toast';
-
 import { useStateContext } from '../context/StateContext';
 import { urlFor } from '../lib/client';
-import getStripe from '../lib/getStripe';
 
 const Cart = () => {
+
+  const handleCheckout = () => {
+
+
+  }//handleCheckout
+  
+  //setting up reference for cart
   const cartRef = useRef();
-  const { totalPrice, totalQuantities, cartItems, setShowCart, toggleCartItemQuanitity, onRemove } = useStateContext();
 
-  const handleCheckout = async () => {
-    const stripe = await getStripe();
-
-    const response = await fetch('/api/stripe', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(cartItems),
-    });
-
-    if(response.statusCode === 500) return;
-    
-    const data = await response.json();
-
-    toast.loading('Redirecting...');
-
-    stripe.redirectToCheckout({ sessionId: data.id });
-  }
+  //fetching data from state context
+  const {totalPrice, totalQuantities, cartItems, setShowCart, toggleCartItemQuanitity, onRemove } = useStateContext();
 
   return (
     <div className="cart-wrapper" ref={cartRef}>
