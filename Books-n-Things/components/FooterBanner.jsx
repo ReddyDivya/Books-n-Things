c1db6c62c73482a455bb3dcd81775c151042338e
Divya <divyareddy0205@gmail.com>
2023-04-30 13:16:07 -0700

    fixing issue

--- Books-n-Things/components/FooterBanner.jsx
+++ Books-n-Things/components/FooterBanner.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Link from 'next/Link';
 import {urlFor}  from '../lib/client.js';
+import { useStateContext } from '../context/StateContext';
 
 const FooterBanner = ({footerBanner : 
   {discount, largeText1, largeText2, saleTime, 
   smallText, midText, desc, product, buttonText, image}}) => {
+
+  //fetching data from state context
+  const {totalPrice, totalQuantities, cartItems, setShowCart, toggleCartItemQuanitity, onRemove } = useStateContext();
 
   return (
     <div className="footer-banner-container">
@@ -19,11 +23,14 @@ const FooterBanner = ({footerBanner :
           <p>{smallText}</p>
           <h3>{midText}</h3>
           <p>{desc}</p>
-          <Link href={`/product/${product}`}>
-              <button type="button">
-                {buttonText}
-              </button>
-          </Link>  
+          <Link href="/">
+            <button
+                type="button"
+                onClick={() => setShowCart(false)}
+            >
+              {buttonText}
+            </button>
+          </Link>
         </div>
         <img src={urlFor(image)} className="footer-banner-image"/>
       </div>
