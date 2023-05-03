9e6ccecdf2579ab46ad65f868b665f06ec75bd30
Divya <divyareddy0205@gmail.com>
2023-05-02 19:50:22 -0700

    code walk

--- Books-n-Things/components/HeroBanner.jsx
+++ Books-n-Things/components/HeroBanner.jsx
@@ -2,22 +2,23 @@ import React from 'react';
 import Link from 'next/Link';//The Link component provides a method of client-side navigation within Next.js applications
 import { urlFor } from '../lib/client';
 
-const HeroBanner = ({ heroBanner }) => {
+const HeroBanner = ({ heroBanner : 
+  {smallText, midText, image, product, buttonText, desc} }) => {
   return (
     <div className="hero-banner-container">
       <div>
-        <p className="beats-solo">{heroBanner.smallText}</p>
-        {/* <h3>{heroBanner.midText}</h3> */}
-        {/* <h1>{heroBanner.midText}</h1> */}
+        <p className="beats-solo">{smallText}</p>
+        {/* <h3>{midText}</h3> */}
+        {/* <h1>{midText}</h1> */}
         <h1>BOOKS</h1>
-        <img src={urlFor(heroBanner.image)} alt="headphones" className="hero-banner-image" />
+        <img src={urlFor(image)} alt="headphones" className="hero-banner-image" />
 
         <div>
-          <Link href={`/product/${heroBanner.product}`}>
-            <button type="button">{heroBanner.buttonText}</button>
+          <Link href={`/product/${product}`}>
+            <button type="button">{buttonText}</button>
           </Link>
           <div className="desc">
-            <p>{heroBanner.desc}</p>
+            <p>{desc}</p>
           </div>
         </div>
       </div>
