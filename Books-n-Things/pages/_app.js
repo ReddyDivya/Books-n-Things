612d76af1ad00ae974821087a44531d37ef25d29
Divya <divyareddy0205@gmail.com>
2023-05-04 16:06:04 -0700

    code walk

--- Books-n-Things/pages/_app.js
+++ Books-n-Things/pages/_app.js
@@ -5,12 +5,15 @@ import '../styles/globals.css';
 import { StateContext } from '../context/StateContext';
 
 function MyApp({ Component, pageProps }) {
+  console.log('props >> '+ pageProps);
   return (
     //Wrapping in a Layout to have a Header, Footer for components
     <StateContext>
       {/* Placing each & every component within Layout.*/}
+      
       <Layout>
-        <Toaster/>
+      {/* Add beautiful notifications */}
+        <Toaster/>  
         {
           /* This component should be inside the Layout*/
           /* Eg: If we are in Product page, this is a Product compoennet*/
