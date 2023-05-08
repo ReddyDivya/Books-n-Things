07c1b961141688828558023162217d98c1054883
Divya <divyareddy0205@gmail.com>
2023-05-08 12:00:45 -0700

    code walk

--- Books-n-Things/components/Layout.jsx
+++ Books-n-Things/components/Layout.jsx
@@ -1,3 +1,8 @@
+/*
+  In next js, If you only have one layout for your entire application(Navbar, Footer), 
+  we can create a Custom App and wrap our application with the layout. 
+  Since the <Layout /> component is re-used when changing pages, its component state will be preserved (e.g. input values).
+*/
 import React from 'react';
 import Head from 'next/head'; //head is same as in HTML. It gives some metadata about website.
 import Navbar from './Navbar';
