b38fcc92eeabc884148db3a3ebb0876e3b153b6b
Divya <divyareddy0205@gmail.com>
2023-05-08 12:10:55 -0700

    code walk

--- Books-n-Things/components/Layout.jsx
+++ Books-n-Things/components/Layout.jsx
@@ -2,6 +2,10 @@
   In next js, If you only have one layout for your entire application(Navbar, Footer), 
   we can create a Custom App and wrap our application with the layout. 
   Since the <Layout /> component is re-used when changing pages, its component state will be preserved (e.g. input values).
+
+  Inside layout, we can fetch data on the client-side using useEffect or a library like SWR(stale-while-revalidate). 
+
+  SWR - components will get a stream of data updates constantly and automatically. & the UI will be always fast and reactive.
 */
 import React from 'react';
 import Head from 'next/head'; //head is same as in HTML. It gives some metadata about website.
