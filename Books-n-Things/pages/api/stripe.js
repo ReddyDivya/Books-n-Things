3525f814d8e2b4c604d64188beaa21615a3b94cf
Divya <divyareddy0205@gmail.com>
2023-05-08 15:21:02 -0700

    code walk

--- Books-n-Things/pages/api/stripe.js
+++ Books-n-Things/pages/api/stripe.js
@@ -16,6 +16,8 @@ const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     try {
+      
+      // Create Checkout Sessions from body params.
       const params = {
         submit_type: 'pay',
         mode: 'payment',
